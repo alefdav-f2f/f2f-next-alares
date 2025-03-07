39f5e0644181b80af57ee661cd217861485a3199
alefdav-f2f <148395662+alefdav-f2f@users.noreply.github.com>
2025-03-07 17:02:57 -0300

    att

--- src/app/components/cards/CardOffer.tsx
+++ src/app/components/cards/CardOffer.tsx
@@ -110,12 +110,12 @@ export default function CardOffer({ offer, params }: any) {
                                 <span className={`text-sm ${offer.blackFriday ? 'text-white' : 'text-main'}`}>{offer?.contents[1]?.name}</span>
                             </div>
 
-                            <div className={`flex mb-2 ${plan.alaresPro ? 'hidden' : ''}`}>
+                            <div className={`flex mb-2 ${offer.alaresPro ? 'hidden' : ''}`}>
                                 <FaDownload className={`w-5 h-5 mr-2 ${offer.blackFriday ? 'text-white' : 'text-main'}`} />
                                 <span className={`text-sm ${offer.blackFriday ? 'text-white' : 'text-main'}`}>{offer?.contents[2]?.name}</span>
                             </div>
 
-                            <div className={`flex mb-2 ${plan.alaresPro ? 'hidden' : ''}`}>
+                            <div className={`flex mb-2 ${offer.alaresPro ? 'hidden' : ''}`}>
                                 <FaUpload className={`w-5 h-5 mr-2 ${offer.blackFriday ? 'text-white' : 'text-main'}`} />
                                 <span className={`text-sm ${offer.blackFriday ? 'text-white' : 'text-main'}`}>{offer?.contents[3]?.name}</span>
                             </div>
